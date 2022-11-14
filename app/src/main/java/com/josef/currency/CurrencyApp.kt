8520589b6f5ecb21c57cc69595c7449580683b86
joseph <kigozijosefed1993@gmail.com>
2022-11-14 21:29:31 +0300

    Initial commit dependency setup

---
+++ app/src/main/java/com/josef/currency/CurrencyApp.kt
@@ -0,0 +1,9 @@
+package com.josef.currency
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class CurrencyApp: Application() {
+
+}
---
+++ app/src/main/java/com/josef/currency/hilt/HiltModules.kt
@@ -0,0 +1,11 @@
+package com.josef.currency.hilt
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@InstallIn(SingletonComponent::class)
+@Module
+object HiltModules {
+
+}
